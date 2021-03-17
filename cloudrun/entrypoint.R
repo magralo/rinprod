a3d9e8129342751edbb58c020aa8d94efbd8222b
magralo <magralo@gmail.com>
2021-03-16 19:24:21 -0500

    cloud run files

--- cloudrun/entrypoint.R
+++ cloudrun/entrypoint.R
@@ -4,4 +4,4 @@ library(promises)
 library(future)
 future::plan("multiprocess")
 
-pr("plumber.R") %>% pr_run(host='0.0.0.0', port = 8000)
+pr("plumber.R") %>% pr_run(host='0.0.0.0', port = as.numeric(Sys.getenv('PORT')) )
